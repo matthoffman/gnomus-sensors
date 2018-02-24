f4e09c8bb1e6a5e7adfb71bab14fe85536bd301b
Matt Hoffman <matt@mhoffman.org>
2018-02-24 17:16:42 -0500

    fixing some bugs, adding some new ones
    running through some of the existing Onewire code in the terminal, and fixing some bugs there.
    Then adding some new methods (or stubs, at least) to connect to other sensors, but haven't tested those yet.

--- seedbeds/main.py
+++ seedbeds/main.py
@@ -1,19 +1,57 @@
-import machine
+from machine import ADC, I2C, unique_id, Pin
+from dht import DHT22
 import time
 import ds18x20
 import onewire
 import config
 import uhttp
-from machine import Pin
 from micropython import alloc_emergency_exception_buf
+
 
 POLL_INTERVAL = 60
 alloc_emergency_exception_buf(100)
 
 
+def initialize_i2c():
+    # construct an I2C bus
+    i2c = I2C(scl=Pin(12), sda=Pin(14), freq=100000)
+    print("Initialized I2C bus. Has the following devices attached: ".format(i2c.scan()))
+    return i2c
+
+
+def read_soil_moisture_adc():
+    return ADC(0).read()
+
+
+def read_soil_moisture_i2c(i2c):
+    """The ADS1115 4-channel ADC converter is somewhere between 0x48-0x4B"""
+    i2c.read("0x48")
+    pass
+
+
 def read_soil_moisture():
-    adc = machine.ADC(0)
+    adc = read_soil_moisture_adc()
     return {"internal": adc}
+
+
+def bytearray_to_str(b):
+    return "".join("{:02x}".format(x) for x in b)
+
+
+def read_humidity_i2c(i2c):
+    """The BME280 temp/humidity sensor is usually on 0x76"""
+    pass
+
+def read_dht22():
+    dht = DHT22(Pin(0))
+    d.measure()
+    d.temperature()
+    d.humidity()
+
+
+def read_luminosity_i2c(i2c):
+    """The TSL2561 luminosity sensor might be listening on 0x39, 0x29, or 0x49 """
+    pass
 
 
 def read_temperature():
@@ -24,7 +62,8 @@ def read_temperature():
     time.sleep_ms(750)
     temps = {}
     for rom in roms:
-        temps[rom] = ds.read_temp(rom)
+        rom_str = bytearray_to_str(rom)
+        temps[rom_str] = ds.read_temp(rom)
     print("Found temps: {}".format(temps))
     return temps
 
@@ -35,8 +74,9 @@ while True:
         # read the sensors
         moisture = read_soil_moisture()
         temp = read_temperature()
+        i2c = initialize_i2c()
         # post it to the server
-        resp = uhttp.post(config.SERVER_URL, {"id": machine.unique_id(),
+        resp = uhttp.post(config.SERVER_URL, {"id": unique_id(),
                                               "version": 1,
                                               "readings": {
                                                   "moisture": moisture,
