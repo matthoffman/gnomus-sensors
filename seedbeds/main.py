199539178524b40748ee88fd29e93be5cd12dfa4
Matt Hoffman <matt@mhoffman.org>
2018-02-24 21:47:22 -0500

    i think everything now working?
    also, change output format from sensors. this one was easier.

--- seedbeds/main.py
+++ seedbeds/main.py
@@ -1,12 +1,13 @@
 from machine import ADC, I2C, unique_id, Pin
 from dht import DHT22
+from ads1x15 import ADS1115
+import bme280
 import time
 import ds18x20
 import onewire
 import config
-import uhttp
+import urest
 from micropython import alloc_emergency_exception_buf
-
 
 POLL_INTERVAL = 60
 alloc_emergency_exception_buf(100)
@@ -14,44 +15,69 @@ alloc_emergency_exception_buf(100)
 
 def initialize_i2c():
     # construct an I2C bus
-    i2c = I2C(scl=Pin(12), sda=Pin(14), freq=100000)
+    i2c = I2C(scl=Pin(12), sda=Pin(14))
     print("Initialized I2C bus. Has the following devices attached: ".format(i2c.scan()))
     return i2c
 
 
 def read_soil_moisture_adc():
-    return ADC(0).read()
+    adc = ADC(0).read()
+    return [{"id": "internal_adc",
+             "value": adc,
+             "type": "soil moisture"}]
 
 
 def read_soil_moisture_i2c(i2c):
-    """The ADS1115 4-channel ADC converter is somewhere between 0x48-0x4B"""
-    i2c.read("0x48")
-    pass
+    """The ADS1115 4-channel ADC converter is somewhere between 0x48-0x4B. Ours is
+      on the default. We're assuming we're only hooking soil sensors to it."""
+    addr = 72
+    adc = ADS1115(i2c, addr, 0)
+    ret = []
+    for i in range(0, 3):
+        val = adc.read(0, i)
+        if val < 3130 or val > 3150:
+            ret.append({"id": "adc_{}_{}".format(addr, i),
+                        "type": "soil moisture",
+                        "value": val})
 
-
-def read_soil_moisture():
-    adc = read_soil_moisture_adc()
-    return {"internal": adc}
-
-
-def bytearray_to_str(b):
-    return "".join("{:02x}".format(x) for x in b)
+    return ret
 
 
 def read_humidity_i2c(i2c):
     """The BME280 temp/humidity sensor is usually on 0x76"""
-    pass
+    bme = bme280.BME280(i2c=i2c)
+    print(bme.values)
+    (temp, pressure, humidity) = bme.read_compensated_data()
+    return [{"id": "bme280_0x76_temp",
+             "value": temp / 100.0,
+             "type": "temperature"},
+            {"id": "bme280_0x76_pressure",
+             "type": "pressure",
+             "value": pressure / 256.0},
+            {"id": "bme280_0x76_humidity",
+             "value": humidity / 1024.0,
+             "type": "humidity"}]
+
 
 def read_dht22():
-    dht = DHT22(Pin(0))
-    d.measure()
-    d.temperature()
-    d.humidity()
+    pin = 0
+    dht = DHT22(Pin(pin))
+    dht.measure()
+    return [{"id": "dht22_{}_temp".format(pin),
+             "value": dht.temperature(),
+             "type": "temperature"},
+            {"id": "dht22_{}_humidity".format(pin),
+             "value": dht.humidity(),
+             "type": "humidity"}]
 
 
 def read_luminosity_i2c(i2c):
     """The TSL2561 luminosity sensor might be listening on 0x39, 0x29, or 0x49 """
     pass
+
+
+def bytearray_to_str(b):
+    return "".join("{:02x}".format(x) for x in b)
 
 
 def read_temperature():
@@ -60,10 +86,12 @@ def read_temperature():
     roms = ds.scan()
     ds.convert_temp()
     time.sleep_ms(750)
-    temps = {}
+    temps = []
     for rom in roms:
         rom_str = bytearray_to_str(rom)
-        temps[rom_str] = ds.read_temp(rom)
+        temps.append({"id": rom_str,
+                      "type": "temperature",
+                      "value": ds.read_temp(rom)})
     print("Found temps: {}".format(temps))
     return temps
 
@@ -72,16 +100,21 @@ def read_temperature():
 while True:
     try:
         # read the sensors
-        moisture = read_soil_moisture()
-        temp = read_temperature()
         i2c = initialize_i2c()
+        moisture = read_soil_moisture_i2c(i2c)
+        # moisture2 = read_soil_moisture_adc()
+        temp = read_temperature()
+        humidity = read_humidity_i2c(i2c)
+        dht_temp = read_dht22()
+
+        readings = moisture + temp + humidity + dht_temp
+
         # post it to the server
-        resp = uhttp.post(config.SERVER_URL, {"id": unique_id(),
+        resp = urest.post(config.SERVER_URL, {"id": bytearray_to_str(unique_id()),
                                               "version": 1,
-                                              "readings": {
-                                                  "moisture": moisture,
-                                                  "temp": temp}})
-        # now maybe do something with the result?
+                                              "readings": readings})
+        # now maybe do something with the result from that POST?
+        # Maybe it told us to do something?
     except Exception as e:
         print("Exception in main loop: {}".format(e))
 
